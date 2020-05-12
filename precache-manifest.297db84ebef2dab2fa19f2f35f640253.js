288a4e6811900edc839d4371913092013b7db2fa
Markandrew Terry <markandrew.terry@gmail.com>
2020-05-12 16:52:35 +0200

    Updates

---
+++ precache-manifest.297db84ebef2dab2fa19f2f35f640253.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575582e75c71b0203d62751e4fe5bc1e",
+    "url": "/rubyredmoon/bonus/index.html"
+  },
+  {
+    "revision": "b9b46afe0de76bb6843d",
+    "url": "/rubyredmoon/bonus/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ba87e00cf9956f2ae394",
+    "url": "/rubyredmoon/bonus/static/js/2.aadef174.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rubyredmoon/bonus/static/js/2.aadef174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b46afe0de76bb6843d",
+    "url": "/rubyredmoon/bonus/static/js/main.1f0c1c8c.chunk.js"
+  },
+  {
+    "revision": "0f4b4e69639362eb1443",
+    "url": "/rubyredmoon/bonus/static/js/runtime-main.e8ea6e83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rubyredmoon/bonus/static/media/logo.5d5d9eef.svg"
+  }
+]);
